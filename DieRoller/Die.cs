1ce55401311a6954e6215554a294d886ed26cd91
MattColaProgrammer93 <mattchoque115@gmail.com>
2022-07-12 09:51:47 -0700

    Created the Die (Single Dice) class.

---
+++ DieRoller/Die.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DieRoller
+{
+    // A single dice that will roll 1-6.
+    internal class Die
+    {
+    }
+}
