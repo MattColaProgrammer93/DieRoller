9a2050465ecc9cfa7a1d5128590763d93d610904
MattColaProgrammer93 <mattchoque115@gmail.com>
2022-07-12 10:07:17 -0700

    Added more properties to the Die class for die value, is the die held, and roll for new face value.

--- DieRoller/Die.cs
+++ DieRoller/Die.cs
@@ -6,8 +6,33 @@ using System.Threading.Tasks;
 
 namespace DieRoller
 {
-    // A single dice that will roll 1-6.
-    internal class Die
+    /// <summary>
+    /// It represents a single six sided die that rolls 1-6
+    /// </summary>
+    public class Die
     {
+        /// <summary>
+        /// The current face up value of the die 
+        /// </summary>
+        public byte FaceValue { get; set; }
+        
+        /// <summary>
+        /// True if the die currently held
+        /// </summary>
+        public bool IsHeld { get; set; }
+        
+        /// <summary>
+        /// Rolls the die and set the <see cref="FaceValue"/> 
+        /// to the new number. Returns the new number
+        /// </summary>
+        /// <returns>Returns the new random number</returns>>
+        public byte Roll()
+        {
+            // Generate random number
+            // Set to face up value
+            // Return new number
+            throw new NotImplementedException();
+        }
+
     }
 }
